1695545ff54355ca71f69874f257d2c4b312c9ec
Anwesh43 <anweshthecool0@gmail.com>
2019-04-02 10:03:12 +0530

    adding CircleToSquareCircle class

--- circletosquarecircleview/src/main/java/com/anwesh/uiprojects/circletosquarecircleview/CircleToSquareCircleView.kt
+++ circletosquarecircleview/src/main/java/com/anwesh/uiprojects/circletosquarecircleview/CircleToSquareCircleView.kt
@@ -173,7 +173,30 @@ class CircleToSquareCircleView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class CircleToSquareCircle(var i : Int) {
+        private val root : CTSCNode = CTSCNode(0)
+        private var curr : CTSCNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 
