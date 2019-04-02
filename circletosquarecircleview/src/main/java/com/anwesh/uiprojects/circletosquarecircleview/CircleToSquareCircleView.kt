6262ea00ee6b909bf15340ac3f01470614109e3f
Anwesh43 <anweshthecool0@gmail.com>
2019-04-02 09:45:51 +0530

    adding drawCTSCNode canvas extension method

--- circletosquarecircleview/src/main/java/com/anwesh/uiprojects/circletosquarecircleview/CircleToSquareCircleView.kt
+++ circletosquarecircleview/src/main/java/com/anwesh/uiprojects/circletosquarecircleview/CircleToSquareCircleView.kt
@@ -28,3 +28,42 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
+
+fun Canvas.drawSemiCircle(y : Float, r : Float, paint : Paint) {
+    save()
+    translate(0f, y)
+    drawArc(RectF(-r, -r, r, r), 0f, 180f, true, paint)
+    restore()
+}
+
+fun Canvas.drawHalfSquare(w : Float, h : Float, paint : Paint) {
+    drawRect(RectF(-w / 2, 0f, w / 2, h), paint)
+}
+
+fun Canvas.drawHalfSquareCircle(sc : Float, size : Float, paint : Paint) {
+    val h : Float = (size / 2) * sc
+    drawSemiCircle(h, size / 2, paint)
+    drawHalfSquare(h, size / 2, paint)
+}
+
+fun Canvas.drawCTSCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    for (j in 0..(rects - 1)) {
+        save()
+        scale(1f, 1f - 2 * j)
+        drawHalfSquareCircle(sc1.divideScale(j, rects), size, paint)
+        restore()
+    }
+    restore()
+}
